b1191759688b8b1f8815ae6338c399d4b8d0e4be
eaubry <elian.aubry@dbit.lu>
2020-09-14 17:38:14 +0200

    [IFRPROJ] creation github action

--- warmup.py
+++ warmup.py
@@ -1,2 +1,38 @@
 import sys
-print(sys.argv[1])
+import boto3
+import time
+
+## Get ASG Name
+asgName = sys.argv[1]
+
+## Set Desired Instance to 1 in ASG
+asgClient = boto3.client('autoscaling')
+asgClient.update_auto_scaling_group(AutoScalingGroupName=asgName, DesiredCapacity=1)
+
+#We get first instance of ASG
+asgDescription = asgClient.describe_auto_scaling_groups(AutoScalingGroupNames=[asgName])
+instancesList = asgDescription['Instances']
+instance = instancesList[0]
+
+
+desiredStatus = 'InService'
+waitingStatus = 'Pending'
+instanceStatus = instance['LifecycleState']
+
+#We wait for inService status
+inService = instanceStatus = instance['LifecycleState'] == desiredStatus
+while ((not inService) and (waitingStatus in instanceStatus)):
+  instanceStatus = instance['LifecycleState']
+  inService = instanceStatus = instance['LifecycleState'] == desiredStatus
+  time.sleep(10)
+
+if instanceStatus == desiredStatus:
+  exit(0)
+else:
+  exit(1)
+
+
+
+
+
+
